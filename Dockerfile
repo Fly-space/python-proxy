e46a803bb5f190af8ca05c34f7bc274a864ed2f8
Evgenchik <miraculumdo@gmail.com>
2023-10-27 21:43:03 +0300

    добавлен unicorn

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.8-slim
 
-RUN pip install Flask requests
+RUN pip install Flask gunicorn requests
 
 COPY . /app
 WORKDIR /app
 
+ENV FLASK_APP=proxy:app
 
-CMD ["python", "proxy.py"]
+CMD ["gunicorn", "-b", "0.0.0.0:80", "proxy:app"]
--- proxy.py
+++ proxy.py
@@ -1,18 +1,29 @@
 from flask import Flask, Response, request
 import requests
+import logging
 
 app = Flask(__name__)
 
-SITE1_URL = 'http://a1'
-SITE2_URL = 'http://a2'
-SITE3_URL = 'http://a3'
+log_file = "./app/app.log"
+
+app.debug = True
+
+handler = logging.FileHandler(log_file)
+
+handler.setLevel(logging.DEBUG)
+
+app.logger.addHandler(handler)
+
+SITE1_URL = 'http://miass.ru'
+SITE2_URL = 'http://74.ru'
+SITE3_URL = 'http://google.ru'
 
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def proxy(path):
     if path == '1':
-        url = f'{SITE1_URL}/api/v1/ntlm'
+        url = f'{SITE1_URL}/actual.html'
     elif path == '2':
-        url = f'{SITE2_URL}/api/v1/ntlm'
+        url = f'{SITE2_URL}/text/world/2023/10/27/72855869'
     else:
         # Если значение параметра пути не '1' и не '2', перенаправляем на SITE3_URL
         url = SITE3_URL
@@ -27,4 +38,4 @@ def proxy(path):
         return Response(status=302, headers={'Location': SITE3_URL})
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
